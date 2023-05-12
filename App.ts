5245596519e92c34b7d2a74d6f1e1a9579afb731
wayne <weijiewang001@gmail.com>
2023-05-12 17:17:27 +1000

    String, boolean, number

---
+++ App.ts
@@ -0,0 +1,25 @@
+let hello = 'hello world';
+
+// console.log(hello)
+
+// 3-1 变量声明
+var number1 = 5;
+let number2 = 2;
+const numhber3 = 3;
+
+
+// 3-3: number, string, boolean
+
+// function add(n1: number, n2: number) {
+//   return n1 + n2;
+// }
+
+// console.log(add(number1, number2))
+
+let isTrue: boolean = true;
+
+// isTrue = 'true';
+
+let  total: number = 0;
+let firstName: string = "wayne"
+let str = `My name is  ${firstName}`
